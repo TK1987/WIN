3cb71dce0dc58ec76e6fd817ef7f530ec84a91eb
TK1987 <45066172+TK1987@users.noreply.github.com>
2020-09-10 16:48:01 +0200

    Update Powershell.ps1

--- Temp/Powershell.ps1
+++ Temp/Powershell.ps1
@@ -8,10 +8,10 @@ $ExcelPfad = (ls -r "$Env:Systemdrive\Program Files*\Microsoft Office\Excel.exe"
     write-host -n "Erstelle Kontextmenüeinträge für '$_'... "
     md -Force              "Registry::HKCU\Software\Classes\$Keyname\shell\Excel\command"                    >$NULL -EA stop
     md -Force              "Registry::HKCU\Software\Classes\$Keyname\shell\Excel (schreibgeschützt)\command" >$NULL -EA stop
-    Set-Itemproperty -Path "Registry::HKCU\Software\Classes\$Keyname\shell\Excel\command"                    -Name '(default)' -Value """$ExcelPfad"" ""%1"""
-    Set-Itemproperty -Path "Registry::HKCU\Software\Classes\$Keyname\shell\Excel (schreibgeschützt)\command" -Name '(default)' -Value """$ExcelPfad"" /r ""%1"""
-    Set-Itemproperty -Path "Registry::HKCU\Software\Classes\$Keyname\shell\Excel"                            -Name 'Icon' -Value """$ExcelPfad"""
-    Set-Itemproperty -Path "Registry::HKCU\Software\Classes\$Keyname\shell\Excel (schreibgeschützt)"         -Name 'Icon' -Value """$ExcelPfad"""
+    Set-Itemproperty -Path "Registry::HKCU\Software\Classes\$Keyname\shell\Excel\command"                    -Name '(default)' -Value """$ExcelPfad"" ""%1"""    -EA stop
+    Set-Itemproperty -Path "Registry::HKCU\Software\Classes\$Keyname\shell\Excel (schreibgeschützt)\command" -Name '(default)' -Value """$ExcelPfad"" /r ""%1""" -EA stop
+    Set-Itemproperty -Path "Registry::HKCU\Software\Classes\$Keyname\shell\Excel"                            -Name 'Icon'      -Value """$ExcelPfad"""           -EA stop
+    Set-Itemproperty -Path "Registry::HKCU\Software\Classes\$Keyname\shell\Excel (schreibgeschützt)"         -Name 'Icon'      -Value """$ExcelPfad"""           -EA stop
     write-host -f green "erfolgreich. "
     } catch {write-host -f red $_.Exception.Message}}
   }
