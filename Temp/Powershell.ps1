3fbef4246c036cf1273e5abf849d455886affa2a
TK1987 <45066172+TK1987@users.noreply.github.com>
2020-09-10 16:46:59 +0200

    Update Powershell.ps1

--- Temp/Powershell.ps1
+++ Temp/Powershell.ps1
@@ -3,15 +3,15 @@ $ExcelPfad = (ls -r "$Env:Systemdrive\Program Files*\Microsoft Office\Excel.exe"
 
 # Kontextmenüeinträge erstellen
 '.xls','.xlsx' | % {
-	$Keyname = (gp "Registry::HKCR\$_" -EA si).'(default)'
-	if ($Keyname) {try {
-		write-host -n "Erstelle Kontextmenüeinträge für '$_'... "
-		md -Force "Registry::HKCU\Software\Classes\$Keyname\shell\Excel\command"                    >$NULL -EA stop
-		md -Force "Registry::HKCU\Software\Classes\$Keyname\shell\Excel (schreibgeschützt)\command" >$NULL -EA stop
-		Set-Itemproperty -Path "Registry::HKCU\Software\Classes\$Keyname\shell\Excel\command"                    -Name '(default)' -Value """$ExcelPfad"" ""%1"""
-		Set-Itemproperty -Path "Registry::HKCU\Software\Classes\$Keyname\shell\Excel (schreibgeschützt)\command" -Name '(default)' -Value """$ExcelPfad"" /r ""%1"""
-		Set-Itemproperty -Path "Registry::HKCU\Software\Classes\$Keyname\shell\Excel"                            -Name 'Icon' -Value """$ExcelPfad"""
-		Set-Itemproperty -Path "Registry::HKCU\Software\Classes\$Keyname\shell\Excel (schreibgeschützt)"         -Name 'Icon' -Value """$ExcelPfad"""
-		write-host -f green "erfolgreich. "
-		} catch {write-host -f red $_.Exception.Message}}
-	}
+  $Keyname = (gp "Registry::HKCR\$_" -EA si).'(default)'
+  if ($Keyname) {try {
+    write-host -n "Erstelle Kontextmenüeinträge für '$_'... "
+    md -Force              "Registry::HKCU\Software\Classes\$Keyname\shell\Excel\command"                    >$NULL -EA stop
+    md -Force              "Registry::HKCU\Software\Classes\$Keyname\shell\Excel (schreibgeschützt)\command" >$NULL -EA stop
+    Set-Itemproperty -Path "Registry::HKCU\Software\Classes\$Keyname\shell\Excel\command"                    -Name '(default)' -Value """$ExcelPfad"" ""%1"""
+    Set-Itemproperty -Path "Registry::HKCU\Software\Classes\$Keyname\shell\Excel (schreibgeschützt)\command" -Name '(default)' -Value """$ExcelPfad"" /r ""%1"""
+    Set-Itemproperty -Path "Registry::HKCU\Software\Classes\$Keyname\shell\Excel"                            -Name 'Icon' -Value """$ExcelPfad"""
+    Set-Itemproperty -Path "Registry::HKCU\Software\Classes\$Keyname\shell\Excel (schreibgeschützt)"         -Name 'Icon' -Value """$ExcelPfad"""
+    write-host -f green "erfolgreich. "
+    } catch {write-host -f red $_.Exception.Message}}
+  }
