eafadb77e391369414aeb70c3c748bdf29ee4a31
TK1987 <tk1987@mailbox.org>
2023-02-28 10:17:41 +0100

    Desktopordner aus [Environment] anstatt $HOME\Desktop bzw. $ENV:Public\Desktop.

--- choco-update.ps1
+++ choco-update.ps1
@@ -1,4 +1,4 @@
-$Runspaces = Foreach ($Dir in $Home,$ENV:Public) {
+$Runspaces = Foreach ($dir in 'Desktop','CommonDesktop') {
   [Powershell]::Create().AddScript({
     Param([String]$Path)
     $Watcher = [System.IO.FileSystemWatcher]::New($Path,'*.lnk')
@@ -6,7 +6,7 @@ $Runspaces = Foreach ($Dir in $Home,$ENV:Public) {
       $LNK = $Watcher.WaitForChanged('Create',1000)
       if (!$LNK.TimedOut) {rm ("{0}\{1}" -f $Path,$LNK.Name)}
     }
-  }).AddParameter('Path',"$Dir\Desktop")
+  }).AddParameter('Path',[Environment]::GetFolderPath($dir))
 }
 [void]$Runspaces.BeginInvoke()
 choco upgrade --limit-output --confirm all
